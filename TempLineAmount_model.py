b356ed8ae22ba94e70cadcfe9c666b28b3f458e4
Shreyas Tripathi <shreyas.tripathi.ece16@itbhu.ac.in>
2019-06-24 16:22:00 +0530

    Update TempLineAmount_model.py

--- TempLineAmount_model.py
+++ TempLineAmount_model.py
@@ -8,4 +8,18 @@ conn = pyodbc.connect('Driver={SQL Server};'
 # cursor = conn.cursor()
 # cursor.execute('select *  from CreditStatsPredictionData')
 # credit_x = excelfile.parse('x_train1')
+
 sql = "select *  from CreditStatsPredictionData"
+credit_x = pd.read_sql(sql,conn)
+credit_x = credit_x.fillna(credit_x.median())
+credit_x.shape
+credit_x = credit_x.drop(['GFP_ID','ParentDescription'], axis = 1)
+
+from datetime import datetime
+chdate_x = []
+for i in range(0, len(credit_x['CHDate'])):
+    j = str(credit_x['CHDate'][i].date())
+    t = datetime.strptime(j, '%Y-%m-%d').toordinal()
+    chdate_x.append(t)
+credit_x = credit_x.assign(CHDate = chdate_x)
+print (credit_x)
